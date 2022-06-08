9b029592e21c427c8f7bb9b79f5a2f0521e4249b
a01081440011 <a01081440011@gmail.com>
2022-06-08 13:49:31 +0900

    chore: test tagging with changesets

--- package/next-web/pages/index.tsx
+++ package/next-web/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => <div>Hello monorepo 🎉</div>;
+const Home: NextPage = () => <div>Hello monorepo🎉</div>;
 
 export default Home;
